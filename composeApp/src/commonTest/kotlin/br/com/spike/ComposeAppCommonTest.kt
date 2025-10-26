5bc66111337422bfde48fd2fba1831c7a15d5568
valdirapn <valdiraires.pn@gmail.com>
2025-10-26 18:23:39 -0300

    initial commit

---
+++ composeApp/src/commonTest/kotlin/br/com/spike/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package br.com.spike
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
